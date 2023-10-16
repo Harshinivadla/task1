acf44fb3456a30fe9b01f9b5773c1344ee9b6610
Harshinivadla <148036297+Harshinivadla@users.noreply.github.com>
2023-10-16 11:28:51 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        
+        int a = scanner.nextInt();
+        float f = scanner.nextFloat();
+
+       
+        scanner.nextLine();
+        
+
+        
+        
+        String s = scanner.nextLine();
+
+        
+        
+        System.out.println( s);
+        System.out.println( f);
+        System.out.println( a);
+
+       
+        
+    }
+}
