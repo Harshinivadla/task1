d0a358c34de2684d27b3f3d4c3ccf245159300d6
Harshinivadla <148036297+Harshinivadla@users.noreply.github.com>
2023-11-02 15:48:16 +0530

    Add files via upload

---
+++ UserDefinedException.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class EvenFactorException extends Exception {
+    public EvenFactorException(String message) {
+        super(message);
+    }
+}
+
+public class UserDefinedException {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        try {
+            if (!(number% 2 == 0)) {
+                throw new EvenFactorException("odd number found");
+            }
+            else{
+                System.out.println("Even number");
+            }
+        } catch (EvenFactorException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
