93a6e7e765cdb40606954899f0295a46caac20a2
Harshinivadla <148036297+Harshinivadla@users.noreply.github.com>
2023-11-27 11:05:15 +0530

    Add files via upload

--- task34FileCSV.java
+++ task34FileCSV.java
@@ -1,8 +1,20 @@
+// Try to create csv file
+// Add FirstName, LastName and mobileNumber inside the csv file.
+
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 
 public class task34FileCSV {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         File filecsv = new File("C:filecsv.csv");
         try {
             if (filecsv.createNewFile()){
@@ -15,5 +27,36 @@ public class task34FileCSV {
             System.out.println("Error occured...");
             e.printStackTrace();
         }
+
+        try (FileWriter fileW = new FileWriter("C:filecsv.csv",true)){
+
+            System.out.println("Enter how many contacts you want to add : ");
+            int choice = input.nextInt();
+            input.nextLine();
+
+            for (int i=0;i<choice ; i++){
+                System.out.println("Enter FirstName: ");
+                String firstname = input.nextLine();
+
+                System.out.println("Enter last name :");
+                String lastname = input.nextLine();
+
+                System.out.println("Enter mobile number :");
+                long mobileNo = input.nextLong();
+                input.nextLine();
+
+                fileW.append(firstname)
+                .append(",")
+                .append(lastname)
+                .append(",")
+                .append(String.valueOf(mobileNo))
+                .append("\n");
+            }
+        }catch(IOException e){
+            e.printStackTrace();;
+        }
     }
 }
+
+
+
